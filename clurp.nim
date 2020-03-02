bef6cd4629133d345334d0eb4e2ae10686e3e4a7
Volodymyr Melnychuk <valdema_r@hotmail.com>
2020-03-02 14:38:59 +0200

    better error handling, fix error (#3)
    * better error handling, fix error
    
    * cleanup
    
    * More detailed error message
    
    Co-authored-by: bromund <60217815+bromund@users.noreply.github.com>

--- clurp.nim
+++ clurp.nim
@@ -60,10 +60,15 @@ proc clurpCmdLine(thisModule: string, paths: openarray[string], includeDirs: ope
     when defined(windows):
         clurp &= ".cmd"
     result = clurp & " wrap --thisModule=" & quoteShell(thisModule) & " --paths="
-    for p in paths: result.add(quoteShell(p) & ":")
+    var pathes: seq[string]
+    for i, p in paths: pathes.add(quoteShell(p))
+    result.add(pathes.join(":"))
+
     if includeDirs.len > 0:
         result &= " --includes="
-        for p in includeDirs: result.add(quoteShell(p) & ":")
+        var includes: seq[string]
+        for p in includeDirs: includes.add(quoteShell(p))
+        result.add(includes.join(":"))
 
 proc nimPathWithCPath(thisModuleDir: string, p: string): string =
     result = thisModuleDir / "clurpcache" / p.extractFilename.changeFileExt("nim")
@@ -85,8 +90,11 @@ template clurp*(paths: static[openarray[string]], includeDirs: static[openarray[
     const thisModuleDir = parentDir(thisModule)
     const cmdLine = clurpCmdLine(thisModule, paths, includeDirs)
     # static: echo "args: ", cmdLine
-    const cmdLineRes = staticExec(cmdLine, cache = cmdLine)
-    # static: echo cmdLineRes
+    const res = gorgeEx(cmdLine, cache = cmdLine)
+    static:
+        when res.exitCode != 0:
+            {.error: "Gorge failed command: " & cmdLine & "\nOutput:" & res.output.}
+
     importClurpPaths(thisModuleDir, paths)
 
 when isMainModule:
@@ -119,7 +127,7 @@ when isMainModule:
         content = content.replace(includePattern) do(m: int, n: int, c: openArray[string]) -> string:
             let header = normalizedIncludePath(c[1])
             # echo "process header ", c, " h ", header
-            if header in ctx.moduleHeaders: return ""
+            if header in ctx.moduleHeaders or header.len == 0: return ""
 
             var fullPath = ""
             for p in ctx.allHeaders:
@@ -136,7 +144,8 @@ when isMainModule:
                     fullPath = ctx.currentPath / header
 
             if fullPath.len > 0:
-                var cnt = readFile(fullPath)
+                var cnt = try: readFile(fullPath)
+                    except: raise newException(Exception, "Can't open header file: " & fullPath)
                 ctx.moduleHeaders.incl(header)
                 let prevPath = ctx.currentPath
                 ctx.currentPath = parentDir(fullPath)
@@ -148,7 +157,7 @@ when isMainModule:
             else:
                 result = c[0]
 
-    proc wrapAUX(thisModule: string, paths: string, includes: string = "") =
+    proc wrapAUX(thisModule: string, paths: string, includes: string = ""):int =
         # echo "called wrap for: ", thisModule
         var paths = paths.split(":")
         let thisModuleDir = parentDir(thisModule)
@@ -160,7 +169,8 @@ when isMainModule:
             if p.isHeaderFile: c.allHeaders.add(thisModuleDir / p)
         for p in paths:
             if not p.isHeaderFile:
-                var src = readFile(thisModuleDir / p)
+                var src = try: readFile(thisModuleDir / p)
+                    except: raise newException(Exception, "Can't open source file: " & thisModuleDir / p)
                 c.currentPath = parentDir(thisModuleDir / p)
                 c.moduleHeaders = initHashSet[string]()
                 preprocessIncludes(src, c)
